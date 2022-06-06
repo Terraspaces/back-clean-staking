8db383a01b132eb1aced9d95bf0efc4ef7d9e4ba
CleverSos <xuguangxia725@gmail.com>
2022-06-06 16:55:45 +0000

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Clean_Staking, Clean_Farming } = require("./script");
 let executionCount = 0;
 const task = cron.schedule("* */60 * * *", async (d) => {
   console.log("date: ", d.toISOString());
-  await Clean_Farming();
+  await Clean_Staking();
 });
 
 const init = async () => {
