4e0a97413034db0c26fa452c9aae6cfe007f6957
Urko <urko.bein@wiserentr.com>
2022-08-26 08:53:55 +0200

    fix cron schedule

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const cron = require("node-cron");
 const { Clean_Staking, Clean_Farming } = require("./script");
 
 let executionCount = 0;
-const clean_staking_task = cron.schedule("* /10  * * * ", async (d) => {
+const clean_staking_task = cron.schedule("*/10 * * * *", async (d) => {
   console.time(Clean_Staking.name);
   await Clean_Staking();
   console.timeEnd(Clean_Staking.name);
 });
 
-const clean_farming_task = cron.schedule("* /10  * * * ", async (d) => {
+const clean_farming_task = cron.schedule("*/10 * * * *", async (d) => {
   console.time(Clean_Farming.name);
   await Clean_Farming();
   console.timeEnd(Clean_Farming.name);
